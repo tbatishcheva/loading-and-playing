aa35a90ef26cc3a909ef14751e27f9cadd52fa1c
Tatyana <>
2021-09-12 13:24:38 +0300

    17.4: Amplitude Analysis - p5.js Sound Tutorial

--- index.js
+++ index.js
@@ -1,49 +1,17 @@
 let song;
-let sliderVolume;
-let sliderRate; // speed
-let sliderPan; // from right ear to left
 let button; // from right ear to left
-let jumpButton; // from right ear to left
-
-// function preload() {
-//     song = loadSound('springtide.mp3');
-// }
+let amp; // amplitude
 
 function setup() {
     createCanvas(200, 200);
-    button = createButton('play');
-    button.mousePressed(togglePlaying);
-
-    jumpButton = createButton('jump');
-    jumpButton.mousePressed(jumpSong);
 
     song = loadSound('springtide.mp3', loaded); // not preload
-    song.setVolume(0.5); // from 0 to 1
-    // slider = createSlider(0, 1, 0.5, 0.01);
-    sliderVolume = createSlider(0, 1, 0.5, 0.01); // from 0 to 1
-    sliderRate = createSlider(0, 3, 1, 0.01);
-    sliderPan = createSlider(-1, 1, 0, 0.01);
-    // song.play(); // not preload
-    // song.stop(); // info
-    // song.loop(); // info
+    song.setVolume(0.3); // from 0 to 1
 
-    // song.addCue(5, changeBackground);
-    song.addCue(2, changeBackground, color(0, 0, 255)); // blue color
-    song.addCue(4, changeBackground, color(0, 255, 255)); // cian color
-    song.addCue(6, changeBackground, color(255, 255, 255)); // white color
-}
+    amp = new p5.Amplitude();
+    // amp.getLevel(); // level of volume
 
-function changeBackground(color) {
-    // background(random(255), random(255), random(255));
-    background(color);
-}
-
-function jumpSong() {
-    const len = song.duration();
-    const t = random(len);
-    // song.jump(len / 2);
-    // song.jump(20); // 20 sec
-    song.jump(t);
+    background(51);
 }
 
 function togglePlaying() {
@@ -57,17 +25,18 @@ function togglePlaying() {
 }
 
 function loaded() {
-    // song.play();
+    console.log('loaded');
+    button = createButton('play');
+    button.mousePressed(togglePlaying);
 }
 
 function draw() {
-    // background(0);
-    song.setVolume(sliderVolume.value());
-    song.rate(sliderRate.value());
-    song.pan(sliderPan.value());
+    background(51);
 
-    // if (song.currentTime() > 5) {
-    //     background(255, 0, 255); // pinkish
-    // }
-    // background(song.currentTime() * 10, 0, 255);
+    const vol = amp.getLevel();
+    const diameter = map(vol, 0, 0.3, 10, 200);
+
+    fill(255, 0, 255);
+    // ellipse(width / 2, height / 2, vol * 2000, vol * 2000);
+    ellipse(width / 2, height / 2, diameter, diameter);
 }
